9e7958b36996739ad925417ed80ea6f804c7c7cf
Kamil Geagea <kamilgeagea@kamils-mbp.wireless.concordia.ca>
2023-01-21 13:25:55 -0500

    implemented modal preview

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,7 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
+import StarIcon from '@mui/icons-material/Star';
+
 import "./Main.scss";
 
 const Main = () => {
+    const [isOpen, setOpen] = useState(true);
+    const sheet = useRef();
     const videoRef = useRef(null);
 
     useEffect(() => {
@@ -13,12 +17,30 @@ const Main = () => {
         });
     }, [videoRef]);
 
-    useEffect(() => {
-        
-    }, []);
-
     return (
         <div className="main">
+            <div className="display">
+                <div className="sheet">
+                    <div className="handle">
+                        <div className="knob" />
+                    </div>
+                    <div className="title-row">
+                        <div className="title">McDonald's</div>
+                        <div className="rating">
+                            <div className="score">4.5</div>
+                            <StarIcon className="star-icon" />
+                        </div>
+                    </div>
+                    <div className="description-row">
+                        <div className="place-type">
+                            <div className="type">Restaurant</div>
+                            <div className="cost"> . $$</div>
+                        </div>
+                        <div className="place-status">Open</div>
+                    </div>
+                </div>
+            </div>
+
             <video
                 width={window.innerWidth}
                 height={window.innerHeight}
